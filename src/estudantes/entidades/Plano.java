a94c0e8c65c6c4199b6e11ae50e0030b1f2614b6
Francisco Braga Marcon <franbragamarcon@proton.me>
2026-08-25 15:08:13 -0300

    Update Plano.java

--- src/estudantes/entidades/Plano.java
+++ src/estudantes/entidades/Plano.java
@@ -10,24 +10,16 @@ public class Plano extends DocumentoAcademico {
 
     public Plano(String criador, CodigoCurso codigoCurso, int paginas,
             long autenticacao, String responsavel, String[] planejamento) {
-        super(criador, codigoCurso, paginas, autenticacao);
-        this.responsavel = responsavel;
-        this.planejamento = planejamento;
-    }
-
+                super(criador, codigoCurso, paginas, autenticacao);
+                this.responsavel = responsavel;
+                this.planejamento = planejamento;
+            }
+            
     @Override
     public boolean equals(Object objeto) {
-        if (!super.equals(objeto)) {
-            return false;
-        }
-        Plano plano = (Plano) objeto;
-        return Objects.equals(responsavel, plano.responsavel)
-                && Arrays.equals(planejamento, plano.planejamento);
     }
 
     @Override
     public int hashCode() {
-        int resultado = Objects.hash(super.hashCode(), responsavel);
-        return 31 * resultado + Arrays.hashCode(planejamento);
     }
 }
